e92c9fecee2f1753d571eb91a60c6c7992608e66
ThiagoVelazquez <velazquezgiulianithiago@gmail.com>
2025-10-21 17:24:49 -0300

    Update sala_model.php

--- tpe_web_2/tpe2/app/models/sala_model.php
+++ tpe_web_2/tpe2/app/models/sala_model.php
@@ -3,8 +3,15 @@ class SalaModel {
     private $db;
 
     public function __construct() {
-        global $db;
-        $this->db = $db;
+        $this->db = new PDO(
+            "mysql:host=".MYSQL_HOST.";dbname=".MYSQL_DB.";charset=utf8mb4", 
+            MYSQL_USER, 
+            MYSQL_PASS,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            ]
+        );
     }
 
     public function getAll() {
